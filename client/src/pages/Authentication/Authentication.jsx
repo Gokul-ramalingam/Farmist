36ad79e731aea3bab55cdfd2d9547388dfad6764
Gokul <gokulramalingam1@gmail.com>
2019-11-22 18:57:46 +0530

    Update in Authentication component

---
+++ client/src/pages/Authentication/Authentication.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import SignUp from '../../components/Signup/Signup.component'
+import SignIn from '../../components/Signin/Signin.component'
+import Title from '../../components/Title/Title.component'
+import './Authentication.scss'
+const Authentication = () => {
+    return(
+    <div className="Authentication">
+        <Title className="titleContainer"/>
+    </div>
+    )
+}
+
+export default Authentication;
