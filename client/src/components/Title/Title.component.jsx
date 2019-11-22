99aa0360fb9b5d37deaff83fa8c4f284b1ce564d
Gokul <gokulramalingam1@gmail.com>
2019-11-22 18:56:37 +0530

    Addition of title component

---
+++ client/src/components/Title/Title.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import AuthenticationImage from '../../assets/authentication.png'
+import './Title.component.scss'
+
+const Title = () =>{
+    return(
+        <h1 className="title">Farmist</h1>
+    )
+}
+
+export default Title;
