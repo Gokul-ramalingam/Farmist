392a374f192a82929d08f3d65d2f1ab7e0e15c74
Gokul <gokulramalingam1@gmail.com>
2019-11-22 18:55:48 +0530

    Update in signup component

---
+++ client/src/components/Signup/Signup.component.jsx
@@ -0,0 +1,14 @@
+import React,{ Component } from 'react'
+
+
+class Signup extends Component{
+   render(){
+       return(
+           <div>
+            Sign Up
+           </div>
+       )
+   }
+}
+
+export default Signup
