fdc8adade3b9a0b4b41f3c10959850bee613ab02
Gokul <gokulramalingam1@gmail.com>
2019-11-23 11:17:00 +0530

    Update in app component

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import Authentication from './pages/Authentication/Authentication'
-
+import './App.css'
 function App() {
   return (
-    <div className="App">
+    <div className="container-fluid App">
         <Authentication />
     </div>
   );
