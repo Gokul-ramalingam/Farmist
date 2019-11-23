d9e166f5b84ee788d3afc2d7bdcb9d6640941d55
Gokul <gokulramalingam1@gmail.com>
2019-11-23 19:22:18 +0530

    Additon of toogle state property

--- client/src/pages/Authentication/Authentication.jsx
+++ client/src/pages/Authentication/Authentication.jsx
@@ -1,18 +1,38 @@
-import React from 'react'
+import React,{ Component } from 'react'
 import SignUp from '../../components/Signup/Signup.component'
 import SignIn from '../../components/Signin/Signin.component'
 import './Authentication.scss'
-const Authentication = () => {
+class Authentication extends Component 
+{
+  constructor(){
+    super();
+    this.state = {
+      toggle : true
+    }
+  }
+
+  handleClick = () =>{
+    this.setState({
+      toggle : !this.state.toggle
+    })
+  }
+
+  render()
+  {
     return(
     <div className="col Authentication">
     <div className="row content">
       <div className="col-md-12 title">
-      <h1 className = "mainTitle" ><b>Farm</b><b className="subTitle">ist</b></h1> 
-                <SignUp />
+      <h1 className = "mainTitle"><b>Farm</b><b className="subTitle">ist</b></h1> 
+       {
+         this.state.toggle?
+         <SignUp toggle={this.handleClick}/> : <SignIn toggle={this.handleClick}/>
+       }
       </div>
       </div>
     </div>
     )
+  }
 }
 
 export default Authentication;
