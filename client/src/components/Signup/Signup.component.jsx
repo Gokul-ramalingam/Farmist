96066790fe17b28276e6dbde5a458fcea645e97f
Gokul <gokulramalingam1@gmail.com>
2019-11-23 17:16:43 +0530

    Update in signup component

--- client/src/components/Signup/Signup.component.jsx
+++ client/src/components/Signup/Signup.component.jsx
@@ -1,11 +1,15 @@
 import React,{ Component } from 'react'
-
+import './Signup.style.scss'
 
 class Signup extends Component{
    render(){
        return(
-           <div>
-            Sign Up
+           <div className="signUp">
+             <input type="text" className="name col-10 col-md-3" name="name" placeholder="Enter name"/>
+             <br />
+             <input type="password" className="city col-10 col-md-3" name="city" placeholder="Enter city"/>
+             <br />
+             <input type="mobile" className="mobile col-10 col-md-3" name="mobile" placeholder="Enter mobile number"/>
            </div>
        )
    }
