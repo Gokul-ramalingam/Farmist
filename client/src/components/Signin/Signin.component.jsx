92e74468fc4239869e92587557f881a8b4cddc6f
Gokul <gokulramalingam1@gmail.com>
2019-11-23 21:30:49 +0530

    Update in signin component

--- client/src/components/Signin/Signin.component.jsx
+++ client/src/components/Signin/Signin.component.jsx
@@ -2,15 +2,24 @@ import React,{ Component } from 'react'
 import './Signin.style.scss'
 
 class Signin extends Component {
+    
+    constructor(){
+        super();
+        this.state = {
+            mobile : ''
+        }
+    }
+
     render(){
+        const mobile = this.state.mobile;
         return(
             <div className="signIn">
-            <input type="mobile" className="mobile col-10 col-md-3" name="mobile" placeholder="Enter mobile number"/>
+            <input type="mobile" className="mobile col-10 col-md-3" name="mobile" value={mobile} placeholder="Enter mobile number"/>
             <br />
             <button className="signin">Sign In</button>
             <br />
             <br />
-            <span className="alternate">Already a Farmist? <b className="toggleSignup" onClick={this.props.toggle}>Register</b></span>
+            <span className="alternate">New to Farmist? <b className="toggleSignup" onClick={this.props.toggle}>Register</b></span>
             </div>
         )
     }
