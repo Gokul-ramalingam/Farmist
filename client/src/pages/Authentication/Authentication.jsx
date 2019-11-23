44ef42851bc6f4247145385f6df62d7f4c9609d7
Gokul <gokulramalingam1@gmail.com>
2019-11-23 15:08:39 +0530

    Update in Authentication component

--- client/src/pages/Authentication/Authentication.jsx
+++ client/src/pages/Authentication/Authentication.jsx
@@ -1,17 +1,16 @@
 import React from 'react'
 import SignUp from '../../components/Signup/Signup.component'
 import SignIn from '../../components/Signin/Signin.component'
-import Title from '../../components/Title/Title.component'
 import './Authentication.scss'
 const Authentication = () => {
     return(
     <div className="col Authentication">
     <div className="row content">
-      <div className="col-md-6 title">
-           Hello World!
+      <div className="col-md-5 title">
+             <h1 className="mainTitle"><b>Farm</b><b className="subTitle">ist</b></h1>
       </div>
-      <div className="col-md-6 auth">
-          Hello World!
+      <div className="col-md-7 auth">
+              <SignUp />
       </div>
       </div>
     </div>
