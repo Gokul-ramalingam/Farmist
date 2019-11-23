ef0695f627b84d3c19bae07ccafa7c3203998f43
Gokul <gokulramalingam1@gmail.com>
2019-11-23 17:16:13 +0530

    Addition of header component

---
+++ client/src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return ( 
+        <div className = "header">
+        <h3 className = "mainTitle" ><b>Farm</b><b className="subTitle">ist</b></h3> 
+        </div>
+    )
+}
+
+export default Header;
