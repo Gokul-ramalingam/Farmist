ec25bb874fab64d72b63f581c24e769dc8369bca
Gokul <gokulramalingam1@gmail.com>
2019-11-24 15:06:06 +0530

    Update in app component

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,12 @@ import { Switch,Route,Redirect } from 'react-router-dom'
 import Authentication from './pages/Authentication/Authentication'
 // import Header from './components/Header/Header'
 import Homepage from './pages/Homepage/Homepage.component'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.scss'
 
 function App() {
   return ( 
-    <div className = "row App">
+    <div className = "App">
     <Switch>
     <Route exact path="/" component={ Homepage } />
     <Route exact path="/authentication" component={ Authentication }/>
