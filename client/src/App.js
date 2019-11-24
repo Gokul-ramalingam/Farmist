72e602e3c16d6be79ddcf557a78003e929ffacc7
Gokul <gokulramalingam1@gmail.com>
2019-11-24 09:53:51 +0530

    Addition of Routes and switch

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import { Switch,Route,Redirect } from 'react-router-dom'
 import Authentication from './pages/Authentication/Authentication'
 // import Header from './components/Header/Header'
+import Homepage from './pages/Homepage/Homepage.component'
 import './App.scss'
 
 function App() {
   return ( 
     <div className = "row App">
-    <Authentication />
+    <Switch>
+    <Route exact path="/" component={ Homepage } />
+    <Route exact path="/authentication" component={ Authentication }/>
+    </Switch>
     </div>
   );
 }
