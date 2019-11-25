f5bdd8955cc0c8b54ec2900f4be60d5767618d33
Gokul <gokulramalingam1@gmail.com>
2019-11-25 23:36:01 +0530

    Addition of Menu and Header component

--- client/src/pages/Homepage/Homepage.component.jsx
+++ client/src/pages/Homepage/Homepage.component.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
-
+import Header from '../../components/Header/Header.component'
+import Menu from '../../components/Menu/Menu.component'
+import './Homepage.component.scss'
 const Homepage = () => {
     return (
         <div>
-            <h1>Hello Homepage</h1>
+        <Menu />
+        <Header />
         </div>
     )
 }
