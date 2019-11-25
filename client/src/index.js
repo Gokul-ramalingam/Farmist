e93facea443b2a0c9b72cc83215ae35673e754cf
Gokul <gokulramalingam1@gmail.com>
2019-11-25 12:32:36 +0530

    Addition of provider and store for redux-logger

--- client/src/index.js
+++ client/src/index.js
@@ -2,13 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
 import * as serviceWorker from './serviceWorker';
+import store from './redux/store'
 
 ReactDOM.render(
-<BrowserRouter>
-    <App />
-</BrowserRouter> , 
+<Provider store={store}>
+    <BrowserRouter>
+         <App />
+    </BrowserRouter> 
+</Provider>, 
     document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
