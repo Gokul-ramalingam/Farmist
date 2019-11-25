4e0b8470f9d83103d502b712821b8599fc56e2c4
Gokul <gokulramalingam1@gmail.com>
2019-11-25 15:23:54 +0530

    Addition of comments

--- client/src/redux/root-reducer.js
+++ client/src/redux/root-reducer.js
@@ -1,5 +1,8 @@
 import { combineReducers } from 'redux'
 
+// Root-reducer has detail of all the reducers 
+// so that it can be easy for store to map
+
 import userReducer from './user/user.reducer'
 
 export default combineReducers({
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,6 +1,10 @@
 import { createStore,applyMiddleware } from 'redux'
 import logger from 'redux-logger'
 
+
+// The store includes rootReducer and logger middleware 
+// from redux-logger which provides state changes details in console
+
 import rootReducer from './root-reducer'
 
 const middleware = [logger]
