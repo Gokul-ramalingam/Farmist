0ba7652889f0be39411e2bfd81f9fc1c0c636048
Gokul <gokulramalingam1@gmail.com>
2019-11-25 10:31:39 +0530

    Update in authentication component

--- client/src/pages/Authentication/Authentication.jsx
+++ client/src/pages/Authentication/Authentication.jsx
@@ -1,22 +1,8 @@
 import React,{ Component } from 'react'
-import SignUp from '../../components/Signup/Signup.component'
 import SignIn from '../../components/Signin/Signin.component'
 import './Authentication.scss'
 class Authentication extends Component 
 {
-  constructor(){
-    super();
-    this.state = {
-      toggle : true
-    }
-  }
-
-  handleClick = () =>{
-    this.setState({
-      toggle : !this.state.toggle
-    })
-  }
-
   render()
   {
     return(
@@ -24,10 +10,7 @@ class Authentication extends Component
     <div className="row content">
       <div className="col-md-12 title">
       <h1 className = "mainTitle"><b>Farm</b><b className="subTitle">ist</b></h1> 
-       {
-         this.state.toggle?
-         <SignUp toggle={this.handleClick}/> : <SignIn toggle={this.handleClick}/>
-       }
+       <SignIn/>
       </div>
       </div>
     </div>
