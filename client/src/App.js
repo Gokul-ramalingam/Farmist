97c24d68af17d9cfaa0c9e7b8a4dbe0916f4b248
Gokul <gokulramalingam1@gmail.com>
2019-11-25 12:35:13 +0530

    Addition of redux functionalities to find user

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,39 @@
-import React from 'react';
+import React,{ Component } from 'react';
 import { Switch,Route,Redirect } from 'react-router-dom'
+import {connect } from 'react-redux'
+import { createStructuredSelector } from 'reselect'
+import { setCurrentUser } from './redux/user/user.action'
+import { selectCurrentUser } from './redux/user/user.selector'
+import { auth } from './components/Firebase/Firebase.util'
 import Authentication from './pages/Authentication/Authentication'
 // import Header from './components/Header/Header'
 import Homepage from './pages/Homepage/Homepage.component'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.scss'
 
-function App() {
+class App extends Component{
+
+  unsubscribe = null;
+
+  componentDidMount(){
+
+    const { setCurrentUser } = this.props;
+    
+    this.unsubscribe = auth.onAuthStateChanged(async user =>{
+
+      if(user)
+        console.log(user)
+      setCurrentUser(user)
+    })
+
+  }
+
+  componentWillUnmount(){
+    this.unsubscribe();
+  }
+
+
+  render(){
   return ( 
     <div className = "App">
     <Switch>
@@ -15,6 +42,15 @@ function App() {
     </Switch>
     </div>
   );
+  }
 }
 
-export default App;
+const mapStateToProps = createStructuredSelector({
+  currentUser : selectCurrentUser
+})
+
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+})
+
+export default connect(mapStateToProps,mapDispatchToProps)(App)
