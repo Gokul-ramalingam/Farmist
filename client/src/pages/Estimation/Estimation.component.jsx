60cc7da93fb4cc5fa2ad555f702c10764714b3a3
Gokul <gokulramalingam1@gmail.com>
2019-11-26 12:10:18 +0530

    Addition of menu,header and estimator component

--- client/src/pages/Estimation/Estimation.component.jsx
+++ client/src/pages/Estimation/Estimation.component.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import Estimator from '../../components/Estimator/Estimator.component'
+import Menu from '../../components/Menu/Menu.component'
+import Header from '../../components/Header/Header.component'
 
 const Estimation = () => {
     return (
         <div>
-            
+            <Menu />
+            <Header />
+            <Estimator />
         </div>
     )
 }
