7b3709ab1c2ceebfe750c982abb04eecc88eaa2c
Gokul <gokulramalingam1@gmail.com>
2019-11-26 10:36:40 +0530

    Update in App component

--- client/src/App.js
+++ client/src/App.js
@@ -1,55 +1,55 @@
 import React,{ Component } from 'react';
 import { Switch,Route,Redirect } from 'react-router-dom'
-import {connect } from 'react-redux'
+import { connect } from 'react-redux'
 import { createStructuredSelector } from 'reselect'
 import { setCurrentUser } from './redux/user/user.action'
 import { selectCurrentUser } from './redux/user/user.selector'
-import { auth } from './components/Firebase/Firebase.util'
+import { auth } from './firebase/firebase.util'
 import Authentication from './pages/Authentication/Authentication'
 // import Header from './components/Header/Header'
 import Homepage from './pages/Homepage/Homepage.component'
 import Estimation from './pages/Estimation/Estimation.component'
 import Prediction from './pages/Prediction/Prediction.component'
 import Availability from './pages/Availability/Availability.component'
-import StockDetails from './pages/Availability/Availability.component'
+import StockDetails from './pages/StockDetails/StockDetails.component'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.scss'
 
-class App extends Component{
+class App extends Component {
 
   unsubscribe = null;
 
-  componentDidMount(){
+  componentDidMount() {
 
     const { setCurrentUser } = this.props;
-    
-    this.unsubscribe = auth.onAuthStateChanged(async user =>{
 
-      if(user)
+    this.unsubscribe = auth.onAuthStateChanged(async user => {
+
+      if (user)
         console.log(user)
       setCurrentUser(user)
     })
 
   }
 
-  componentWillUnmount(){
+  componentWillUnmount() {
     this.unsubscribe();
   }
 
 
-  render(){
-  return ( 
-    <div className = "App">
-    <Switch>
-    <Route exact path="/" component={ Homepage } />
-    <Route exact path="/authentication" render={()=> this.props.currentUser?(<Redirect to='/' />):(<Authentication />)}/>
-    <Route exact path="/estimate" component={ Estimation } />
-    <Route exact path="/predict" component={ Prediction } />
-    <Route exact path="/check" component={ Availability } />
-    <Route exact path="/view" component={ StockDetails } />
-    </Switch>
-    </div>
-  );
+  render() {
+    return ( 
+      <div className = "App" >
+      <Switch >
+      <Route exact path = "/" component = { Homepage }/> 
+      <Route exact path = "/authentication" render = { () => this.props.currentUser ? ( < Redirect to = '/' / > ) : ( < Authentication / > ) }/> 
+      <Route exact path = "/estimate" component = { Estimation }/> 
+      <Route exact path = "/predict" component = { Prediction } />
+      <Route exact path = "/check" component = { Availability } /> 
+      <Route exact path = "/view" component = { StockDetails }/>
+       </Switch> 
+       </div>
+    );
   }
 }
 
@@ -57,7 +57,7 @@ class App extends Component{
 // implements memoization functionality so that processing cost get reduced
 
 const mapStateToProps = createStructuredSelector({
-  currentUser : selectCurrentUser
+  currentUser: selectCurrentUser
 })
 
 
@@ -67,4 +67,4 @@ const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 })
 
-export default connect(mapStateToProps,mapDispatchToProps)(App)
+export default connect(mapStateToProps, mapDispatchToProps)(App)
