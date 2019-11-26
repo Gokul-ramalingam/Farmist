ef6feb9fdf3a0b07098450e73f5bd44be53919b3
Gokul <gokulramalingam1@gmail.com>
2019-11-26 23:37:25 +0530

    Addition and Implementation of Estimation component layout

--- client/src/pages/Estimation/Estimation.component.jsx
+++ client/src/pages/Estimation/Estimation.component.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import Estimator from '../../components/Estimator/Estimator.component'
 import Menu from '../../components/Menu/Menu.component'
 import Header from '../../components/Header/Header.component'
+import './Estimation.style.scss'
 
 const Estimation = () => {
     return (
-        <div>
+        <div className="estimation">
             <Menu />
             <Header />
             <Estimator />
