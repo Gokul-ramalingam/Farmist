d40cd50f5617e6f1ba3b085051978049768006b4
Gokul <gokulramalingam1@gmail.com>
2019-11-26 12:11:08 +0530

    Addition of stockdetails component

--- client/src/pages/StockDetails/StockDetails.component.jsx
+++ client/src/pages/StockDetails/StockDetails.component.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import StockView from '../../components/StockView/StockView.component'
+import Menu from '../../components/Menu/Menu.component'
+import Header from '../../components/Header/Header.component'
 
 const StockDetails = () => {
     return (
         <div>
-            
+            <Menu />
+            <Header />
+            <StockView />
         </div>
     )
 }
