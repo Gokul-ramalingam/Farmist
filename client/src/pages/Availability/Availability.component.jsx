7e523b10101ea76a50926fff6074dbe6ed70415d
Gokul <gokulramalingam1@gmail.com>
2019-11-26 12:10:47 +0530

    Addition of menu,header and checker component

--- client/src/pages/Availability/Availability.component.jsx
+++ client/src/pages/Availability/Availability.component.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import Checker from '../../components/Checker/Checker.component'
+import Menu from '../../components/Menu/Menu.component'
+import Header from '../../components/Header/Header.component'
 
 const Availability = () => {
     return (
         <div>
-            
+        <Menu />
+        <Header />
+        <Checker />
         </div>
     )
 }
