319351ea304d6722f3945b629b8ae4f3685cb9be
Gokul <gokulramalingam1@gmail.com>
2019-11-26 12:09:42 +0530

    Update in prediction component

--- client/src/pages/Prediction/Prediction.component.jsx
+++ client/src/pages/Prediction/Prediction.component.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import Predictor from '../../components/Predictor/Predictor.component'
+import Menu from '../../components/Menu/Menu.component'
+import Header from '../../components/Header/Header.component'
 
 const Prediction = () => {
     return (
         <div>
-            
+        <Menu />
+        <Header />
+        <Predictor />
         </div>
     )
 }
