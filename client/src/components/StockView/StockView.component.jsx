45563b06a1cbc5dbee73ce460660ce2f46b7eefd
Gokul <gokulramalingam1@gmail.com>
2019-11-27 19:57:38 +0530

    Update in stock view component

--- client/src/components/StockView/StockView.component.jsx
+++ client/src/components/StockView/StockView.component.jsx
@@ -1,12 +1,22 @@
-import React from 'react'
+import React,{ useState } from 'react'
 import './StockView.style.scss'
 import Chart from '../Chart/Chart.component'
+import Input from '../Input/Input.component'
 
 const StockView = (props) => {
+
+const [ crop,setCrop ] = useState('');
+
+let handleChange = (event) =>{
+ setCrop(event.target.value)
+}
+
     return (
         <div className="stockView">
-             <h3 className="stockType">{ props.children }</h3>
-             <Chart />
+             <Input change={handleChange} crop={crop}/>
+             <Chart title= "Stock In City" type="Doughnut"/>
+             <Chart title= "Stock In State" type="Bar"/>
+             <Chart title= "Stock In Country" type="Pie"/>
         </div>
     )
 }
