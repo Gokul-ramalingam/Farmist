8aed3590aa624591bf4531068c96ccb8b5422605
Gokul <gokulramalingam1@gmail.com>
2019-11-28 13:57:25 +0530

    Addition of router functionality

--- client/src/pages/Welcome/Welcome.component.jsx
+++ client/src/pages/Welcome/Welcome.component.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
 import Background from '../../assets/background1.mp4'
 import './Welcome.component.scss'
-const Welcome = () => {
+import { withRouter } from 'react-router-dom'
+
+const Welcome = (props) => {
     return (
         <div>
         <video src={Background} id="home-bg-video" autoPlay loop muted></video>
-             <h1 className = "mainTitle"><b>Farm</b><b className="subTitle">ist</b></h1> 
+             <h1 className = "mainTitle1"><b>Farm</b><b className="subTitle1">ist</b></h1> 
              <h2 className="text"><b>Get twice the productivity, 
              <br/>we assist you to manage 
              <br/>your day to day activity</b></h2>
-             <button className="details">Start Managing</button>
+             <button className="details" onClick={()=>  props.history.push('/')}>Start Managing</button>
         </div>
     )
 }
 
-export default Welcome
+export default withRouter(Welcome)
