07452b6eb437e37b89017c31be4d5bc1eaf02db3
Gokul <gokulramalingam1@gmail.com>
2019-11-28 17:25:25 +0530

    Implementation of server setup

---
+++ server/server.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const bodyparser = require('body-parser')
+const cors = require('cors')
+
+const app = express()
+
+const port = process.env.PORT || 7000
+
+//MongoDB Configuration
+const db = require('./config/connection').url
+
+//Middleware
+app.use(bodyparser.urlencoded({extended:false}))
+app.use(bodyparser.json())
+app.use(cors())
+
+//Mongoose Connection
+mongoose.connect(db,{useNewUrlParser:true,useUnifiedTopology:true})
+                    .then(() => {
+                        console.log('Connected to MongoDB successfully...')
+                    })
+                    .catch(err => console.log('Error occured while try to establish connection with MongoDB '+err))
+
+
+//Listening to port
+app.listen(port,() => {
+    console.log(`App is running at port ${port}`)
+})
