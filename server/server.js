9e6010b2aac9846f79f2307c25a6a2f6784c37fe
Gokul <gokulramalingam1@gmail.com>
2019-11-28 18:28:53 +0530

    Addition of new routes

--- server/server.js
+++ server/server.js
@@ -7,6 +7,9 @@ const app = express()
 
 const port = process.env.PORT || 7000
 
+//routes
+const estimate = require('./routes/api/estimate')
+
 //MongoDB Configuration
 const db = require('./config/connection').url
 
@@ -22,10 +25,12 @@ mongoose.connect(db,{useNewUrlParser:true,useUnifiedTopology:true})
                     })
                     .catch(err => console.log('Error occured while try to establish connection with MongoDB '+err))
 
-//Main route
 app.get('/',(req,res)=>{
     res.send('Server is up and running')
 })
+
+//Main route
+app.use('/api/estimate',estimate)
 
 //Listening to port
 app.listen(port,() => {
