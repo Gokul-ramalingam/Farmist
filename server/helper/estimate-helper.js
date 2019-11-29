8adde6d6371d20bab6e3d65474872b9a8e2518eb
Gokul <gokulramalingam1@gmail.com>
2019-11-30 04:15:07 +0530

    Update in estimate helper

--- server/helper/estimate-helper.js
+++ server/helper/estimate-helper.js
@@ -1,5 +1,18 @@
-const generateEstimate = (data) => {
-console.log(data)
+const findData = require('../services/EstimationService').findData
+
+const generateEstimate = (data,cropArray) => {
+   const { crop,currentprice,city,duration,activities } = data
+   let datas = []; 
+
+   cropArray.forEach(crop =>{
+       crop.activities.reduce((a,b) => {
+            a.machinary + b.machinary   
+       },0)
+   })
+    //  let val = cropArray.activities.reduce =>((a,b)=>{
+    //         a.machinary + b.machinary
+    //  },0)
+     console.log(cropArray);
 }
 
 module.exports = { generateEstimate }
