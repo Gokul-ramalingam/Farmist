9645739b49b4a55be454f009d4d367a4c6729253
Gokul <gokulramalingam1@gmail.com>
2019-11-30 11:22:26 +0530

    Update in predictor component

--- client/src/components/Predictor/Predictor.component.jsx
+++ client/src/components/Predictor/Predictor.component.jsx
@@ -1,13 +1,50 @@
 import React,{ Component } from 'react'
 import axios from 'axios'
+import Spinner from 'react-bootstrap/Spinner'
+import './Predictor.style.scss'
+
 class Predictor extends Component {
+
+constructor(){
+    super()
+    this.state = {
+        result : [],
+        toggle: false
+    }
+}
+
    componentDidMount(){
        axios.get(`http://localhost:7000/api/predict/details`)
+                 .then(result => this.setState({
+                     result : result.data,
+                     toggle : !this.state.toggle
+                 }))
+                 .catch(err => this.setState({
+                     toggle : !this.state.toggle
+                 }))
    }
+
    render(){
+       let items
+       if(this.state.result.length > 0)
+        items = this.state.result.map((item,index) => <li key={index} className="list">{item.split("-").reverse().join('-')}</li>)
+       else
+       items = <h3>For the next five days there will be continous rain</h3>
     return (
         <div>
-            <h1>Hello Predictor</h1>
+        {
+            this.state.toggle? 
+            <div className="predictionContainer">
+            <h2>Suggested Days</h2>
+                 {items}
+            </div>
+            : 
+            <div className="loadingScreen">
+               <Spinner animation="border" role="status">
+                  <span className="sr-only">Loading...</span>
+              </Spinner>
+            </div>
+        }
         </div>
     )
    }
