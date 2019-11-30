6ce5bbae0076aa3b1243976618ba9c93d650b4ae
Gokul <gokulramalingam1@gmail.com>
2019-11-30 10:23:17 +0530

    addition of axios

--- client/src/components/Estimator/Estimator.component.jsx
+++ client/src/components/Estimator/Estimator.component.jsx
@@ -1,11 +1,27 @@
 import React,{ useState } from 'react'
+import axios from 'axios'
+import EstimationResult from '../EstimatorResult/EstimatorResult.component'
 import './Estimator.style.scss'
 
 const Estimator = (props) => {
     const [ cropName,setCropName ] = useState('');
     const [ noOfAcres,setNoOfAcres ] = useState('');
+    const [ toggle, setToggle ] = useState(true);
+    const [ result, setResult ] = useState({});
+
+ let handleSubmit = () => {
+    axios.get(`http://localhost:7000/api/estimate/find/${cropName}/${noOfAcres}`)
+              .then((result) =>{
+                  setResult(result)
+                  setToggle(false)
+              })
+              .catch(err => console.log(err))
+ }
 
     return (
+        <div>
+        {
+         toggle?
         <div className="estimator">
             <h4 className="title"><b>Estimator</b></h4>
             <input type="text" 
@@ -20,8 +36,11 @@ const Estimator = (props) => {
             value={ noOfAcres } 
             onChange={ event => setNoOfAcres(event.target.value) }
             placeholder="Number of acres" /><br/>
-            <button id="estimateButton" className="estimateButton">Estimate</button>
+            <button id="estimateButton" className="estimateButton" onClick={handleSubmit}>Estimate</button>
         </div> 
+        : <EstimationResult data={result.data}/>
+        }
+        </div>
     )
 }
 
