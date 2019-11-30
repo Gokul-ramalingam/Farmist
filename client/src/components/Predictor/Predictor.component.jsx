946834a40670b7cd8117da7e173aeec584ab60a5
Gokul <gokulramalingam1@gmail.com>
2019-11-30 10:24:08 +0530

    Update in predictor component

--- client/src/components/Predictor/Predictor.component.jsx
+++ client/src/components/Predictor/Predictor.component.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
-
-const Predictor = () => {
+import React,{ Component } from 'react'
+import axios from 'axios'
+class Predictor extends Component {
+   componentDidMount(){
+       axios.get(`http://localhost:7000/api/predict/details`)
+   }
+   render(){
     return (
         <div>
             <h1>Hello Predictor</h1>
         </div>
     )
+   }
 }
 
 export default Predictor
