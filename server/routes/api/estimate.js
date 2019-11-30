2e66ff43774bf05ebc6e36ea826f1db99b1d05bb
Gokul <gokulramalingam1@gmail.com>
2019-11-30 10:25:20 +0530

    Update in estimate route

--- server/routes/api/estimate.js
+++ server/routes/api/estimate.js
@@ -1,14 +1,23 @@
 const express = require('express')
 const router = express.Router()
-const createEstimatePlan = require('../../services/EstimationService').createEstimatePlan
+const { createEstimatePlan,checkAvailabilityPlan }  = require('../../services/EstimationService')
 
 // @type                       POST
 // @route                     api/estimate/calculate
 // @description           This route is for estimating investment and turnover
 // @access                  PRIVATE
 
-router.post('/calculate',async  (req,res) =>{
-     await createEstimatePlan(req.body,res)
+router.post('/calculate/',async  (req,res) =>{
+     await createEstimatePlan(req,res)
+})
+
+// @type                       GET
+// @route                     api/estimate/find
+// @description           This route is for estimating investment and turnover based on existing data
+// @access                  PRIVATE
+
+router.get('/find/:crop/:acres',async (req,res) => {
+     await checkAvailabilityPlan(req,res)
 })
 
 
